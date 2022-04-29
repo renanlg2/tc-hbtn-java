7acda42f6a4f598ae457e3d497e6d1928235cac6
Renan de Lima Gomes <renanlg@ciandt.com>
2022-04-29 14:47:28 -0300

    Task 5

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor: R$ %,8.2f \nTaxa: %.2f%%\n", valor, taxa);
+    }
+}
