b8f0763e65b43bf3cc1788daac365f80a6f71a73
Renan de Lima Gomes <renanlg@ciandt.com>
2022-04-29 14:20:23 -0300

    Tasks 3

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,7 @@
+public class PrintStringVariable {
+    public static void main(String[] args){
+        String texto = "Holberton School";
+        System.out.printf("%s\n%s\n%s", texto, texto, texto.substring(0,9));
+    }
+}
+
