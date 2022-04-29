ffc52e72bec0006b1597f504347536d6bf9f794f
Renan de Lima Gomes <renanlg@ciandt.com>
2022-04-29 13:54:42 -0300

    Tasks 0, 1 e 2

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,9 @@
+public class PrintIntegerVariable {
+
+    public static void main (String[] args){
+        int numero = 311;
+
+        System.out.println("Avenida Andaló, " + numero);
+    }
+
+}
