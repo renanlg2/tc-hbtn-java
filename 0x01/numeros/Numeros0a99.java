b44777a1e97c9ddf07f5ccd8c123d84734038cd2
Renan de Lima Gomes <renanlg@ciandt.com>
2022-04-29 15:39:01 -0300

    Task 0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,11 @@
+public class Numeros0a99 {
+    public static void main(String[] args){
+        for (int i = 0; i <= 99; i++){
+            if (i != 99){
+                System.out.print(i + ", ");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
