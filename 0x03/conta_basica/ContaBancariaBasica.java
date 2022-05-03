869c795acbb826b7665373029ebed43a0a06aff1
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-03 15:27:01 -0300

    .

---
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,68 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+    private String numeracao;
+    private double saldo;
+    private double taxaJurosAnual;
+
+    public String getNumeracao(){
+        return this.numeracao;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public double getTaxaJurosAnual(){
+        return this.taxaJurosAnual;
+    }
+
+    public ContaBancariaBasica(String numeracao, double taxaJurosAnual){
+        this.numeracao = numeracao;
+        this.taxaJurosAnual = taxaJurosAnual;
+        this.saldo = 0;
+    }
+
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        if (valor > 0){
+            this.saldo += valor;
+        } else {
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+        }
+    }
+
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor > 0){
+
+            if (valor <= getSaldo()){
+                this.saldo -= valor;
+            } else {
+                throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+            }
+
+        } else {
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que 0");
+        }
+    }
+
+    public double calcularTarifaMensal(){
+        if ((getSaldo() * 0.10) < 10){
+            return getSaldo() * 0.10;
+        } else {
+            return 10;
+        }
+    }
+
+    public double calcularJurosMensal(){
+        if (getSaldo() > 0){
+            return getSaldo() * ((getTaxaJurosAnual() / 12) / 100);
+        } else {
+            return 0;
+        }
+    }
+
+    public void aplicarAtualizacaoMensal(){
+        this.saldo += calcularJurosMensal() - calcularTarifaMensal();
+    }
+
+}
--- 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
+++ 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -1,7 +1,7 @@
 package exceptions;
 
 public class OperacaoInvalidaException extends Exception {
-    OperacaoInvalidaException(String message){
+    public OperacaoInvalidaException(String message){
         super(message);
     }
 }
