3b4f690a604cb3b7fc2776ad667b189b18a3169e
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-04 19:17:59 -0300

    Heranca Fixacao Task 1

---
+++ 0x06/pedido_livraria/produtos/Livro.java
@@ -0,0 +1,43 @@
+package produtos;
+
+public class Livro extends Produto {
+    private int paginas;
+    private String autor;
+    private int edicao;
+
+    public double obterPrecoLiquido(){
+        return super.getPrecoBruto() + (super.getPrecoBruto() * 0.15);
+    }
+
+    public int getPaginas(){
+        return this.paginas;
+    }
+
+    public void setPaginas(int paginas){
+        this.paginas = paginas;
+    }
+
+    public String getAutor(){
+        return this.autor;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    public int getEdicao(){
+        return this.edicao;
+    }
+
+    public void setEdicao(int edicao){
+        this.edicao = edicao;
+    }
+
+    public Livro(String titulo, int ano, String pais, double precoBruto, int paginas, String autor, int edicao){
+        super(titulo, ano, pais, precoBruto);
+        setPaginas(paginas);
+        setAutor(autor);
+        setEdicao(edicao);
+    }
+
+}
