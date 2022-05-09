ecd5e4f9306ad2916224e80136b22454f0138ad0
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-09 15:54:19 -0300

    Relacionamento Entre Classes - Task 0 - Empregado/Endereço

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,43 @@
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void apresentar(){
+        System.out.println("Codigo: " + this.getCodigo());
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Bairro: " + this.endereco.getBairro());
+        System.out.println("Cidade: " + this.endereco.getCidade());
+        System.out.println("Pais: " + this.endereco.getPais());
+    }
+}
