a371355924d70ee796739c025db6b8cd2418133a
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-12 12:00:40 -0300

    Ajustes nas classes que vão trazer a Exceção

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -2,31 +2,27 @@ public class InteiroPositivo {
     private int valor;
 
     public InteiroPositivo(int valor) throws IllegalArgumentException{
-        setValor(valor);
+        this.setValor(valor);
     }
 
     public InteiroPositivo(String valor) throws IllegalArgumentException{
-        setValor(new Integer(valor));
+        this.setValor(Integer.parseInt(valor)); // Convertendo String para int com Wrapper
     }
 
     public int getValor() {
-        return valor;
+        return this.valor;
     }
 
     public void setValor(int valor) throws IllegalArgumentException{
-        if (valor >= 0){
-            this.valor = valor;
-        } else {
+        if (valor < 0)
             throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
-        }
+
+        this.valor = valor;
     }
 
     public boolean ehPrimo(){
-        if (this.valor <= 1) {
+        if (this.valor <= 1)
             return false;
-        }
-
-        boolean ehPrimo = true;
 
         for (int i = 2; i < this.valor; i++ ){
             if (this.valor % i == 0){
