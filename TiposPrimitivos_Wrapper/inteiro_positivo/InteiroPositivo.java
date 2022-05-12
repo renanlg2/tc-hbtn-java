6be0aba022112712026e372c9d885ae214e5831e
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-12 10:43:47 -0300

    Projeto de Tipos Primitivos e Wrapper

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,40 @@
+public class InteiroPositivo {
+    private int valor;
+
+    public InteiroPositivo(int valor){
+        setValor(valor);
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public InteiroPositivo(String valor){
+        Integer valorInt = new Integer(valor);
+        setValor(valorInt);
+    }
+
+    public void setValor(int valor) {
+        if (valor >= 0){
+            this.valor = valor;
+        } else {
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+    }
+
+    public boolean ehPrimo(){
+        if (this.valor == 1){
+            return false;
+        }
+
+        boolean ehPrimo = true;
+        for (int i = 2; i < this.valor; i++ ){
+            if (this.valor % i == 0){
+                ehPrimo = false;
+                break;
+            }
+        }
+
+        return ehPrimo;
+    }
+}
