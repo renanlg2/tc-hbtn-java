c614cb9c158e503eb96f272af3f113e668d5e140
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-17 17:46:41 -0300

    Java - Collections (Set e Map) - 1. Duplicados

---
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class NumerosDuplicados {
+
+    public static TreeSet<Integer> buscar(int[] numeros){
+        TreeSet<Integer> numerosDuplicados = new TreeSet<Integer>();
+        HashSet<Integer> hashNumeros = new HashSet<Integer>();
+
+        for (Integer numero : numeros){
+            if (!hashNumeros.add(numero)){
+                numerosDuplicados.add(numero);
+            }
+        }
+
+        return numerosDuplicados;
+    }
+}
