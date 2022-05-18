d40a63306f6635759ba2f4b51e07561c2dd7784f
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-18 15:10:23 -0300

    Java - Collections (Set e Map) - 3. Telefones Duplicados

---
+++ Set_Map/telefones_duplicados/ListaTelefonica.java
@@ -0,0 +1,52 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class ListaTelefonica {
+    private HashMap<String, HashSet<Telefone>> dadosPessoais;
+
+    public ListaTelefonica() {
+        this.dadosPessoais = new HashMap<String, HashSet<Telefone>>();
+    }
+
+    public HashSet<Telefone> adicionarTelefone(String nome, Telefone telefone){
+        HashSet<Telefone> telefoneLista;
+
+        // Get the Telefone HashSet list
+        telefoneLista = dadosPessoais.get(nome);
+
+        // Validate if HashSet was created
+        if (telefoneLista == null) {
+            telefoneLista = new HashSet<Telefone>();
+        }
+
+        // Validate if the number exists to another people
+        if (telefoneLista.contains(telefone))
+            throw new IllegalArgumentException("Telefone jah existente para essa pessoa");
+
+        // Validate if the number exists to another people
+        Iterator<HashSet<Telefone>> iteratorTelefone = dadosPessoais.values().iterator();
+        while (iteratorTelefone.hasNext()){
+            for ( Telefone telIndividual : iteratorTelefone.next()){
+                if (telIndividual.equals(telefone)){
+                    throw new IllegalArgumentException("Telefone jah pertence a outra pessoa");
+                }
+            }
+        }
+
+        telefoneLista.add(telefone);
+        dadosPessoais.put(nome, telefoneLista);
+
+        return telefoneLista;
+    }
+
+    public HashSet<Telefone> buscar(String nome){
+        HashSet<Telefone> telefoneLista = dadosPessoais.get(nome);
+
+        if (telefoneLista != null)
+            return telefoneLista;
+
+        return null;
+    }
+}
---
+++ Set_Map/telefones_duplicados/Telefone.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+import java.util.TreeSet;
+
+public class Telefone {
+    private String codigoArea;
+    private String numero;
+
+    public Telefone(String codigoArea, String numero) {
+        this.codigoArea = codigoArea;
+        this.numero = numero;
+    }
+
+    public String getCodigoArea() {
+        return codigoArea;
+    }
+
+    public void setCodigoArea(String codigoArea) {
+        this.codigoArea = codigoArea;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + codigoArea + ") " + numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Telefone telefone = (Telefone) o;
+        return this.getCodigoArea() != null && this.getNumero() != null && this.getCodigoArea().equals(telefone.getCodigoArea()) && this.getNumero().equals(telefone.getNumero());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoArea, numero);
+    }
+}
