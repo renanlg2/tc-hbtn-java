cb16446c004918b29c0e1a98c4e127ce92f39afd
Renan de Lima Gomes <renanlg@ciandt.com>
2022-05-27 08:54:17 -0300

    Java - Lambda Expressions - 0. Produto MarkUp

--- Lambda/markup/Produto.java
+++ Lambda/markup/Produto.java
@@ -1,19 +1,18 @@
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class Produto {
+public class Produto{
     private String nome;
     private double preco;
-    private double percentualMarkup;
+    private double percentualMarkup = 10;
 
     public Supplier<Double> precoComMarkUp = () -> this.preco + (this.preco * this.percentualMarkup / 100);
 
-    public Consumer<Double> atualizarMarkUp = valor -> this.percentualMarkup = valor;
+    public Consumer<Double> atualizarMarkUp = (Double valor) -> this.percentualMarkup = valor;
 
     public Produto(double preco, String nome) {
         this.preco = preco;
         this.nome = nome;
-        this.percentualMarkup = 10;
     }
 
     public String getNome() {
