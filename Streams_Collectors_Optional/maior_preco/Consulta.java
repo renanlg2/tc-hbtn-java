99616cc5e83e2e0ee1f842ce1b3f0018514c3dfd
Renan de Lima Gomes <renanlg@ciandt.com>
2022-06-01 13:35:39 -0300

    Java - Streams, Collectors e Optional - 1. Relatório Produto com maior preço

--- Streams_Collectors_Optional/maior_preco/Consulta.java
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -14,7 +14,9 @@ public class Consulta {
     public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
         return produtos
                 .stream()
-                .max(Comparator.comparing(Produto::getPreco))
-                .get();
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .limit(1)
+                .collect(Collectors.toList())
+                .get(0);
     }
 }
