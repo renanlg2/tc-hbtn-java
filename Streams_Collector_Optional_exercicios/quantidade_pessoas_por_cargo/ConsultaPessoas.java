0e2e30c847b9c4c9feedf06a3faa991eb22aa03c
Renan de Lima Gomes <renanlg@ciandt.com>
2022-06-03 08:55:31 -0300

    Java - Streams, Collectors e Optional - Exercícios de fixação - 1. Contagem de pessoas por cargo

--- Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -1,19 +1,24 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class ConsultaPessoas {
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
-
-
-        return
-            pessoas
-            .stream()
+        return pessoas.stream()
             .collect(
-              Collectors.groupingBy(
+              groupingBy(
                       Pessoa::getCargo,
                       () -> new TreeMap<>(Comparator.reverseOrder()),
                       Collectors.toCollection(TreeSet::new))
             );
+
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas){
+        return pessoas.stream()
+                .collect(groupingBy(Pessoa::getCargo, counting()));
 
     }
 
