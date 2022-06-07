89701b6fb1a8d9203311edfdcb0e629872af7843
renanlg2 <104364888+renanlg2@users.noreply.github.com>
2022-06-07 09:02:02 -0300

    Delete 0x00/email directory

--- 0x00/email/Pessoa.java
+++
@@ -1,8 +0,0 @@
-public class Pessoa {
-    public static boolean emailValid(String email){
-        if (email.contains("@") && email.length() <= 50)
-            return true;
-
-        return false;
-    }
-}
